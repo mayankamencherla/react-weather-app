ba2ff661640b8fba7881d642fa77f33120a7f4ac
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-09 23:26:10 +0530

    Minor improvements

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-weather-app
+A basic weather app that makes requests to openWeatherMap api to get weather of your city. 
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Express server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Express server is running on port ${PORT}`);
 });
