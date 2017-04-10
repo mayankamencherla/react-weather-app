a5f0e68a0856dd013aaa61dfd888fad5c83bd8c2
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-11 01:23:35 +0530

    Styled the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,27 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var About = (props) => <h3>About</h3>;
+var About = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>Making this app was a great experience. I learnt the ins and outs of the React.js framework. I also learnt some HTML, CSS and the foundation framework which I used to style the app. I have highlighted some of the skills needed to make this app yourself down below:</p>
+            <ol>
+                <li>
+                    <a href='http://eloquentjavascript.net'>JavaScript</a>
+                </li>
+                <li>
+                    <a href='https://facebook.github.io/react/tutorial/tutorial.html'>React.js library</a>
+                </li>
+                <li>
+                    <a href='https://www.codecademy.com/learn/web'>HTML/CSS</a>
+                </li>
+                <li>
+                    <a href='http://foundation.zurb.com/sites/docs/'>Foundation CSS framework</a>
+                </li>
+            </ol>
+        </div>
+    );
+}
 
 module.exports = About;
