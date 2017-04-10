46f27d41d72a3b7e7898912c3ed8bfbf14fab0c2
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-11 01:14:19 +0530

    Added example links for some locations

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var Examples = (props) => <h3>Examples</h3>;
+var Examples = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>;  
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Philadelphia'>Philadelphia</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Bangalore'>Bangalore</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Boston'>Boston</Link>
+                </li>
+            </ol>
+        </div>
+    );
+}
 
 module.exports = Examples;
